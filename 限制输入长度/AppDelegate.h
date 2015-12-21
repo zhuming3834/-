4f6f4b9fbdd7507158ce52763f052f274670dc23
zhuming <zhuming@zhumingdeMac.local>
2015-12-21 20:59:11 +0800

    Initial Commit

---
+++ 限制输入长度/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  限制输入长度
+//
+//  Created by zhuming on 15/12/21.
+//  Copyright (c) 2015年 zhuming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 限制输入长度/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  限制输入长度
+//
+//  Created by zhuming on 15/12/21.
+//  Copyright (c) 2015年 zhuming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
